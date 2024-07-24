6368224e66ce1b58d4bc167296c672f515d26277
Hussain Ali <50552217+hs622@users.noreply.github.com>
2024-07-24 20:21:56 +0500

    auth controller code indentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post, Request, UseGuards, UseInterceptors, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Request,
+  UseGuards,
+  UseInterceptors,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersService } from '../users/users.service'; 
+import { UsersService } from '../users/users.service';
 import { LocalGuard } from './guards/local.guard';
 import { RegisterInput } from './auth.input';
-import { LoggingInterceptor } from 'src/interceptors/logging/logging.interceptor';
 
 @Controller('auth')
-@UseInterceptors(LoggingInterceptor)
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
@@ -15,11 +21,14 @@ export class AuthController {
 
   @Post('/login')
   @UseGuards(LocalGuard)
-  async getLogin(@Request() Request) {    
-    const token = await this.authService.generateToken(Request.user.id, Request.user.role)
+  async getLogin(@Request() Request) {
+    const token = await this.authService.generateToken(
+      Request.user.id,
+      Request.user.role,
+    );
     return {
-      'user': Request.user,
-      ...token
+      user: Request.user,
+      ...token,
     };
   }
 
