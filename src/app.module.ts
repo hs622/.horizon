9587ee193e02380561f9393a4f48259df5f4e0e2
Hussain Ali <50552217+hs622@users.noreply.github.com>
2024-07-26 15:19:56 +0500

    global interceptors implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,33 @@
-import { Module, UseInterceptors } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod, UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
-import { CompanyService } from './company/company.service';
-import { CompanyController } from './company/company.controller';
-import { CompanyModule } from './company/company.module';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { CompaniesModule } from './companies/companies.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { RoutesMiddleware } from './middlewares/routes.middleware';
+import { APP_PIPE } from '@nestjs/core'; 
 
 @Module({
-  imports: [UsersModule, CompanyModule, AuthModule],
-  controllers: [UsersController, CompanyController, AuthController],
-  providers: [PrismaService, UsersService, CompanyService, AuthService],
+  imports: [AppModule, UsersModule, AuthModule, CompaniesModule],
+  controllers: [AppController, UsersController, AuthController],
+  /**
+   * For remaider: 
+   * APP_PIPE is a token that represents the global pipe instance. We can use it to override the default global pipe with our custom pipe.
+   * 
+   */
+  providers: [{provide: APP_PIPE, useClass: ValidationPipe }, AppService, PrismaService, UsersService, AuthService],
 })
 
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RoutesMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    })
+  }
+}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  private routes = [];
+
+  onModuleInit() {
+    const server = this.httpAdapterHost.httpAdapter.getInstance();
+    const uniqueRoutes = new Set();
+
+    server._router.stack.forEach((layer) => {
+      if (layer.route) {
+        const path = layer.route.path;
+        const method = Object.keys(layer.route.methods)[0].toUpperCase();
+        const routeLayer = `${method} - ${path}`;
+
+        if (!uniqueRoutes.has(routeLayer)) {
+          uniqueRoutes.add(routeLayer);
+          this.routes.push(routeLayer);
+        }
+      }
+    });
+
+    // .filter(layer => layer.route)
+    // .map(layer => this.layerRoute(layer));
+    // .map(layer => Object.keys(layer.route.methods)[0].toUpperCase() +' - '+ '/api'+layer.route.path );
+  }
+
+  layerRoute(layer) {
+    if (layer) console.log(layer.route);
+    return (
+      Object.keys(layer.route.methods)[0].toUpperCase() +
+      ' - ' +
+      '/api' +
+      layer.route.path
+    );
+  }
+
+  getRoutes() {
+    return this.routes;
   }
 }
